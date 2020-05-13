3a93343f665fef6568bfdfb40d643a97ee2f0cf6
nikolay <nikolay@TeamCity>
2020-05-13 22:11:09 +0000

    TeamCity change in 'FourthDemo / Application' project: general settings of 'Deployment' build template were updated

--- .teamcity/Application/buildTypes/Application_Deployment.kt
+++ .teamcity/Application/buildTypes/Application_Deployment.kt
@@ -4,4 +4,8 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 
 object Application_Deployment : Template({
     name = "Deployment"
+
+    enablePersonalBuilds = false
+    type = BuildTypeSettings.Type.DEPLOYMENT
+    maxRunningBuilds = 1
 })
