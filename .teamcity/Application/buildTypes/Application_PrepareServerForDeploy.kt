acda969b0bb49032ff85855f850fe0743e6d341b
nikolay <nikolay@TeamCity>
2020-05-14 22:18:40 +0000

    TeamCity change in 'FourthDemo / Application' project: runners of 'Prepare server for deploy' build configuration were updated

--- .teamcity/Application/buildTypes/Application_PrepareServerForDeploy.kt
+++ .teamcity/Application/buildTypes/Application_PrepareServerForDeploy.kt
@@ -1,6 +1,7 @@
 package Application.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
 object Application_PrepareServerForDeploy : BuildType({
     name = "Prepare server for deploy"
@@ -11,5 +12,18 @@ object Application_PrepareServerForDeploy : BuildType({
 
     vcs {
         root(_Self.vcsRoots.Dp185DevOpsPRiS)
+    }
+
+    steps {
+        script {
+            name = "Update env-templates"
+            scriptContent = """
+                echo "Download current IPs"
+                scp 
+                IP_APP=${'$'}( cat IPs/vm_ip_pub.txt )
+                IP_APP=${'$'}( cat IPs/vm_ip_pub.txt )
+                ssh %username%@
+            """.trimIndent()
+        }
     }
 })
