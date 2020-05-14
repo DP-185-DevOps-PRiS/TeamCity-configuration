8b764adeebb944a76c92f61873fcf01e84512a27
nikolay <nikolay@TeamCity>
2020-05-14 21:28:59 +0000

    TeamCity change in 'FourthDemo / Terraform' project: runners of 'Infrastructure creation' build configuration were updated

--- .teamcity/Terraform/buildTypes/Terraform_InfrastructureCreation.kt
+++ .teamcity/Terraform/buildTypes/Terraform_InfrastructureCreation.kt
@@ -2,15 +2,18 @@ package Terraform.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.exec
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
 object Terraform_InfrastructureCreation : BuildType({
     name = "Infrastructure creation"
 
     steps {
-        exec {
-            name = "Init"
-            path = "terraform"
-            arguments = "init"
+        script {
+            name = "Init & Apply"
+            scriptContent = """
+                terraform init
+                terraform apply -auto-approve
+            """.trimIndent()
         }
         exec {
             name = "Apply"
