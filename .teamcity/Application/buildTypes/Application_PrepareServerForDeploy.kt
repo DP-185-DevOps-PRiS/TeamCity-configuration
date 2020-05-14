7d72d0613f98225719ce059d76f23e7aa3b71eab
nikolay <nikolay@TeamCity>
2020-05-14 22:54:03 +0000

    TeamCity change in 'FourthDemo / Application' project: parameters of 'Prepare server for deploy' build configuration were updated

--- .teamcity/Application/buildTypes/Application_PrepareServerForDeploy.kt
+++ .teamcity/Application/buildTypes/Application_PrepareServerForDeploy.kt
@@ -10,6 +10,7 @@ object Application_PrepareServerForDeploy : BuildType({
         password("database_username", "credentialsJSON:cf6455f3-e3ea-471f-8248-9bd2f420a755", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("database_password", "credentialsJSON:bbb265da-3d7d-49c7-9a25-76240f4408d6", display = ParameterDisplay.HIDDEN, readOnly = true)
         param("repo", "Deployment")
+        password("env_templates_path", "credentialsJSON:02a4c958-c2cb-4f38-9542-a2a57c6dd06f", display = ParameterDisplay.HIDDEN, readOnly = true)
     }
 
     vcs {
