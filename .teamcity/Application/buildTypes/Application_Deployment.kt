d2906566b186c0215544c8f347177aa28d37ddb6
nikolay <nikolay@TeamCity>
2020-05-14 21:35:02 +0000

    TeamCity change in 'FourthDemo / Application' project: parameters of 'Deployment' build template were updated

--- .teamcity/Application/buildTypes/Application_Deployment.kt
+++ .teamcity/Application/buildTypes/Application_Deployment.kt
@@ -11,7 +11,6 @@ object Application_Deployment : Template({
 
     params {
         param("repo", "Deployment")
-        password("username", "credentialsJSON:da22594b-73c4-4496-afd6-71512fc907e7", display = ParameterDisplay.HIDDEN, readOnly = true)
     }
 
     vcs {
