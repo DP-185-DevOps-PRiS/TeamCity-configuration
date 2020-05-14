7c565bfb1de7f9f446fa683c7d080f806b17607b
nikolay <nikolay@TeamCity>
2020-05-14 21:56:26 +0000

    TeamCity change in 'FourthDemo / Application' project: parameters of 'Prepare server for deploy' build configuration were updated

--- .teamcity/Application/buildTypes/Application_PrepareServerForDeploy.kt
+++ .teamcity/Application/buildTypes/Application_PrepareServerForDeploy.kt
@@ -5,6 +5,10 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 object Application_PrepareServerForDeploy : BuildType({
     name = "Prepare server for deploy"
 
+    params {
+        param("repo", "Deployment")
+    }
+
     vcs {
         root(_Self.vcsRoots.Dp185DevOpsPRiS)
     }
