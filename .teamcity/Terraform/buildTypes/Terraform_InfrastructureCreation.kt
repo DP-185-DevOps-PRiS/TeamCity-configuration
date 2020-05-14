8c044e9962dbda55062253af8272de36a744e9c5
nikolay <nikolay@TeamCity>
2020-05-14 21:21:11 +0000

    TeamCity change in 'FourthDemo / Terraform' project: runners of 'Infrastructure creation' build configuration were updated

--- .teamcity/Terraform/buildTypes/Terraform_InfrastructureCreation.kt
+++ .teamcity/Terraform/buildTypes/Terraform_InfrastructureCreation.kt
@@ -17,5 +17,17 @@ object Terraform_InfrastructureCreation : BuildType({
             path = "terraform"
             arguments = "apply"
         }
+        step {
+            name = "Save IP addresses"
+            type = "ssh-deploy-runner"
+            param("jetbrains.buildServer.deployer.sourcePath", """
+                db_ip.txt
+                vm_ip_priv.txt
+                vm_ip_pub.txt
+            """.trimIndent())
+            param("jetbrains.buildServer.deployer.targetUrl", "~/IPs")
+            param("jetbrains.buildServer.sshexec.authMethod", "DEFAULT_KEY")
+            param("jetbrains.buildServer.deployer.ssh.transport", "jetbrains.buildServer.deployer.ssh.transport.scp")
+        }
     }
 })
