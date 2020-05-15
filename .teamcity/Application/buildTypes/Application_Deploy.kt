73ad044c74f2b86a2eaab75b6f1a8aee7672266b
nikolay <nikolay@TeamCity>
2020-05-15 12:03:30 +0000

    TeamCity change in 'FourthDemo / Application' project: runners of 'Deploy' build template were updated

--- .teamcity/Application/buildTypes/Application_Deploy.kt
+++ .teamcity/Application/buildTypes/Application_Deploy.kt
@@ -1,6 +1,7 @@
 package Application.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
 object Application_Deploy : Template({
     name = "Deploy"
@@ -8,4 +9,21 @@ object Application_Deploy : Template({
     enablePersonalBuilds = false
     type = BuildTypeSettings.Type.DEPLOYMENT
     maxRunningBuilds = 1
+
+    steps {
+        script {
+            name = "Run deploy.sh"
+            id = "RUNNER_4"
+            scriptContent = """
+                echo "Downloading current IPs ..."
+                scp -i /root/.ssh/.tc/id_rsa -r %username_tc%@%ip_tc%:~/IPs/vm_ip_pub.txt .
+                
+                echo "Setting IP variables ..."
+                IP_APP_PUBLIC=${'$'}( cat IPs/vm_ip_pub.txt )
+                
+                echo "Run deploy.sh ..."
+                ssh %username_app%@${'$'}IP_APP_PUBLIC "cd /opt/kickscooter && sudo bash deploy.sh %container%"
+            """.trimIndent()
+        }
+    }
 })
