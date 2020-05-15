41d0091570e97bba4e6167e657607c03f760b54a
nikolay <nikolay@TeamCity>
2020-05-15 10:47:57 +0000

    TeamCity change in 'FourthDemo / Terraform' project: parameters of 'Create infrastructure' build configuration were updated

--- .teamcity/Terraform/buildTypes/Terraform_InfrastructureCreation.kt
+++ .teamcity/Terraform/buildTypes/Terraform_InfrastructureCreation.kt
@@ -7,6 +7,7 @@ object Terraform_InfrastructureCreation : BuildType({
     name = "Create infrastructure"
 
     params {
+        password("username_tc", "credentialsJSON:d7e1c6cc-bd2f-4ac0-879c-af7105fe4370", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("ip_tc", "credentialsJSON:60cc8de1-2624-4259-b539-7063000c6f44", display = ParameterDisplay.HIDDEN, readOnly = true)
     }
 
