7f1ff5021112f1ff28feadde59f8d38ff6ef906e
nikolay <nikolay@TeamCity>
2020-05-15 12:13:49 +0000

    TeamCity change in 'FourthDemo / Application / Trip' project: triggers of 'Deploy' build configuration were updated

--- .teamcity/Application_Trip/buildTypes/Application_Trip_Deploy.kt
+++ .teamcity/Application_Trip/buildTypes/Application_Trip_Deploy.kt
@@ -1,6 +1,7 @@
 package Application_Trip.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.finishBuildTrigger
 
 object Application_Trip_Deploy : BuildType({
     templates(Application.buildTypes.Application_Deploy)
@@ -8,6 +9,14 @@ object Application_Trip_Deploy : BuildType({
 
     params {
         param("container", "trip")
+    }
+
+    triggers {
+        finishBuildTrigger {
+            id = "TRIGGER_23"
+            buildType = "${Application_Trip_BuildPushImageToAcr.id}"
+            successfulOnly = true
+        }
     }
 
     dependencies {
