042d242da3c557195c296eba45a27c13cfe67331
nikolay <nikolay@TeamCity>
2020-05-15 10:47:46 +0000

    TeamCity change in 'FourthDemo / Terraform' project: parameters of 'Create infrastructure' build configuration were updated

--- .teamcity/Terraform/buildTypes/Terraform_InfrastructureCreation.kt
+++ .teamcity/Terraform/buildTypes/Terraform_InfrastructureCreation.kt
@@ -6,6 +6,10 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 object Terraform_InfrastructureCreation : BuildType({
     name = "Create infrastructure"
 
+    params {
+        password("ip_tc", "credentialsJSON:60cc8de1-2624-4259-b539-7063000c6f44", display = ParameterDisplay.HIDDEN, readOnly = true)
+    }
+
     steps {
         script {
             name = "Init & Apply"
