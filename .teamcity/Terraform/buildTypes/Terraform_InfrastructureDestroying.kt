83b0300d7c54a65afb8570dcd7108d91f53b1f3c
nikolay <nikolay@TeamCity>
2020-05-15 17:58:50 +0000

    TeamCity change in 'FourthDemo / Terraform' project: VCS roots of 'Destroy infrastructure' build configuration were updated

--- .teamcity/Terraform/buildTypes/Terraform_InfrastructureDestroying.kt
+++ .teamcity/Terraform/buildTypes/Terraform_InfrastructureDestroying.kt
@@ -6,6 +6,10 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.exec
 object Terraform_InfrastructureDestroying : BuildType({
     name = "Destroy infrastructure"
 
+    vcs {
+        root(_Self.vcsRoots.Dp185DevOpsPRiS)
+    }
+
     steps {
         exec {
             name = "Destroy"
