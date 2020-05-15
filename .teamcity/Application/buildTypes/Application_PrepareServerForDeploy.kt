998b9f857c67ef2a5e8d1a88b27765d1fca48f98
nikolay <nikolay@TeamCity>
2020-05-15 00:57:52 +0000

    TeamCity change in 'FourthDemo / Application' project: runners of 'Prepare server for deploy' build configuration were updated

--- .teamcity/Application/buildTypes/Application_PrepareServerForDeploy.kt
+++ .teamcity/Application/buildTypes/Application_PrepareServerForDeploy.kt
@@ -22,10 +22,10 @@ object Application_PrepareServerForDeploy : BuildType({
         password("secret_key", "credentialsJSON:38c2872a-77ec-4818-835b-62ea6642d340", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("mail_password", "credentialsJSON:4ec2f34f-a6be-4705-b82d-f99fece188d4", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("username_tc", "credentialsJSON:d7e1c6cc-bd2f-4ac0-879c-af7105fe4370", display = ParameterDisplay.HIDDEN, readOnly = true)
-        password("env_templates_path", "credentialsJSON:02a4c958-c2cb-4f38-9542-a2a57c6dd06f", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("env.VEHICLE_DB", "credentialsJSON:21138bb2-8e3b-48ee-989d-a1179216fe08", display = ParameterDisplay.HIDDEN, readOnly = true)
-        password("send_mail", "credentialsJSON:689f6d3a-b429-4952-b808-aaa52d462995", display = ParameterDisplay.HIDDEN, readOnly = true)
+        password("env_templates_path", "credentialsJSON:02a4c958-c2cb-4f38-9542-a2a57c6dd06f", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("env.TRIP_DB", "credentialsJSON:27c3ca79-1dd0-4242-9c9a-1c2fe711cd54", display = ParameterDisplay.HIDDEN, readOnly = true)
+        password("send_mail", "credentialsJSON:689f6d3a-b429-4952-b808-aaa52d462995", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("ip_tc", "credentialsJSON:60cc8de1-2624-4259-b539-7063000c6f44", display = ParameterDisplay.HIDDEN, readOnly = true)
     }
 
@@ -49,12 +49,12 @@ object Application_PrepareServerForDeploy : BuildType({
                 mkdir env && chmod 700 env
                 cp %env_templates_path%/kafka.env env
                 sed "s|eureka|${'$'}IP_APP_PRIVATE|" %env_templates_path%/gateway-template.env > env/gateway.env
-                sed "s|ip|${'$'}IP_DB|; s|DB|"identity"|; s|database_username|%database_username%|; s|database_password|%database_password%|; s|eureka|${'$'}IP_APP_PRIVATE|" %env_templates_path%/identity-template.env > env/identity.env
-                sed 's/ip/${'$'}IP_DB/; s/DB/trip/; s/database_username/%database_username%/; s/database_password/%database_password%/; s/eureka/${'$'}IP_APP_PRIVATE/; s/token/%token%/' %env_templates_path%/trip-template.env > env/trip.env
-                sed 's/ip/${'$'}IP_DB/; s/DB/vehicle/; s/database_username/%database_username%/; s/database_password/%database_password%/; s/eureka/${'$'}IP_APP_PRIVATE/; s/token/%token%/' %env_templates_path%/vehicle-template.env > env/vehicle.env
-                sed 's/ip/${'$'}IP_DB/; s/DB/simulator/; s/database_username/%database_username%/; s/database_password/%database_password%/; s/eureka/${'$'}IP_APP_PRIVATE/; s/token/%token%/' %env_templates_path%/simulator-template.env > env/simulator.env
-                sed 's/ip/${'$'}IP_DB/; s/DB/payment/; s/database_username/%database_username%/; s/database_password/%database_password%/; s/pub_key/%pub_key%/; s/secret_key/%secret_key%/; s/eureka/${'$'}IP_APP_PRIVATE/' %env_templates_path%/payment-template.env > env/payment.env
-                sed 's/send_mail/%send_mail%/; s/mail_password/%mail_password%/; s/ip/${'$'}IP_DB/; s/DB/messaging/; s/database_username/%database_username%/; s/database_password/%database_password%/; s/eureka/${'$'}IP_APP_PRIVATE/; s/token/%token%/' %env_templates_path%/messaging-template.env > env/messaging.env
+                sed "s|ip|${'$'}IP_DB|; s|DB|%env.IDENTITY_DB%|; s|database_username|%database_username%|; s|database_password|%database_password%|; s|eureka|${'$'}IP_APP_PRIVATE|" %env_templates_path%/identity-template.env > env/identity.env
+                sed "s|ip|${'$'}IP_DB|; s|DB|%env.TRIP_DB%|; s|database_username|%database_username%|; s|database_password|%database_password%|; s|eureka|${'$'}IP_APP_PRIVATE|; s|token|%token%|" %env_templates_path%/trip-template.env > env/trip.env
+                sed "s|ip|${'$'}IP_DB|; s|DB|%env.VEHICLE_DB%|; s|database_username|%database_username%|; s|database_password|%database_password%|; s|eureka|${'$'}IP_APP_PRIVATE|; s|token|%token%|" %env_templates_path%/vehicle-template.env > env/vehicle.env
+                sed "s|ip|${'$'}IP_DB|; s|DB|%env.SIMULATOR_DB%|; s|database_username|%database_username%|; s|database_password|%database_password%|; s|eureka|${'$'}IP_APP_PRIVATE|; s|token|%token%|" %env_templates_path%/simulator-template.env > env/simulator.env
+                sed "s|ip|${'$'}IP_DB|; s|DB|%env.PAYMENT_DB%|; s|database_username|%database_username%|; s|database_password|%database_password%|; s|pub_key|%pub_key%|; s|secret_key|%secret_key%|; s|eureka|${'$'}IP_APP_PRIVATE|" %env_templates_path%/payment-template.env > env/payment.env
+                sed "s|send_mail|%send_mail%|; s|mail_password|%mail_password%|; s|ip|${'$'}IP_DB|; s|DB|%env.MESSAGING_DB%|; s|database_username|%database_username%|; s|database_password|%database_password%|; s|eureka|${'$'}IP_APP_PRIVATE|; s|token|%token%|" %env_templates_path%/messaging-template.env > env/messaging.env
                 
                 echo "Creating a directory for the applicaion ..."
                 ssh %username_app%@${'$'}IP_APP_PUBLIC "sudo chown  %username_app% /opt && mkdir -p /opt/kickscooter"
