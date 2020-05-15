f508cfac12f885601c193a2b28e48fc4c5baf4af
nikolay <nikolay@TeamCity>
2020-05-15 11:40:46 +0000

    TeamCity change in 'FourthDemo / Application' project: triggers of 'Deploy' build configuration were updated

--- .teamcity/Application/buildTypes/Application_Deploy.kt
+++ .teamcity/Application/buildTypes/Application_Deploy.kt
@@ -4,6 +4,7 @@ import Application_Discovery.buildTypes.Application_Discovery_BuildPushImageToAc
 import Application_Gateway.buildTypes.Application_Gateway_BuildPushImageToAcr
 import Application_Identity.buildTypes.Application_Identity_BuildPushImageToAcr
 import Application_Messaging.buildTypes.Application_Messaging_BuildPushImageToAcr
+import Application_Payment.buildTypes.Application_Payment_BuildPushImageToAcr
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.finishBuildTrigger
 
@@ -29,6 +30,10 @@ object Application_Deploy : BuildType({
         }
         finishBuildTrigger {
             buildType = "${Application_Messaging_BuildPushImageToAcr.id}"
+            successfulOnly = true
+        }
+        finishBuildTrigger {
+            buildType = "${Application_Payment_BuildPushImageToAcr.id}"
             successfulOnly = true
         }
     }
