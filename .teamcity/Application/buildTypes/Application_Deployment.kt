93d97724f1d99092f0055acd0965b582b0d6f977
nikolay <nikolay@TeamCity>
2020-05-15 09:59:50 +0000

    TeamCity change in 'FourthDemo / Application' project: runners of 'Deployment' build template were updated

--- .teamcity/Application/buildTypes/Application_Deployment.kt
+++ .teamcity/Application/buildTypes/Application_Deployment.kt
@@ -11,20 +11,6 @@ object Application_Deployment : Template({
 
     steps {
         step {
-            name = "Upload needed files"
-            id = "RUNNER_7"
-            type = "ssh-deploy-runner"
-            param("jetbrains.buildServer.deployer.username", "%username%")
-            param("jetbrains.buildServer.deployer.sourcePath", """
-                ~/env
-                deploy.sh
-                docker-compose.yml
-            """.trimIndent())
-            param("jetbrains.buildServer.deployer.targetUrl", "%vm_ip%:/tmp")
-            param("jetbrains.buildServer.sshexec.authMethod", "DEFAULT_KEY")
-            param("jetbrains.buildServer.deployer.ssh.transport", "jetbrains.buildServer.deployer.ssh.transport.scp")
-        }
-        step {
             name = "Run deploy.sh"
             id = "RUNNER_8"
             type = "ssh-exec-runner"
