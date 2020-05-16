786059aa7c6c2ffbf04083466dba937ef9917b28
nikolay <nikolay@TeamCity>
2020-05-16 14:44:34 +0000

    TeamCity change in 'FourthDemo / Terraform' project: triggers of 'Create infrastructure' build configuration were updated

--- .teamcity/Terraform/buildTypes/Terraform_InfrastructureCreation.kt
+++ .teamcity/Terraform/buildTypes/Terraform_InfrastructureCreation.kt
@@ -2,6 +2,7 @@ package Terraform.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.retryBuild
 
 object Terraform_InfrastructureCreation : BuildType({
     name = "Create infrastructure"
@@ -10,8 +11,8 @@ object Terraform_InfrastructureCreation : BuildType({
         password("env.ARM_SUBSCRIPTION_ID", "credentialsJSON:21757c12-89df-4120-b4d8-c701b7a6b42b", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("env.ARM_TENANT_ID", "credentialsJSON:3a8befbc-d61b-4c90-ac44-e813a0a459b1", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("ip_tc", "credentialsJSON:60cc8de1-2624-4259-b539-7063000c6f44", display = ParameterDisplay.HIDDEN, readOnly = true)
-        password("username_tc", "credentialsJSON:d7e1c6cc-bd2f-4ac0-879c-af7105fe4370", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("env.ARM_CLIENT_ID", "credentialsJSON:7f676f25-1a15-4eac-81a3-ebc80417c816", display = ParameterDisplay.HIDDEN, readOnly = true)
+        password("username_tc", "credentialsJSON:d7e1c6cc-bd2f-4ac0-879c-af7105fe4370", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("env.ARM_CLIENT_SECRET", "credentialsJSON:53dbf002-6b58-46b5-b977-d6bd992c1b48", display = ParameterDisplay.HIDDEN, readOnly = true)
     }
 
@@ -32,6 +33,12 @@ object Terraform_InfrastructureCreation : BuildType({
                 echo "Sending IPs ..."
                 scp db_ip.txt vm_ip_priv.txt %username_tc%@%ip_tc%:/root/IPs
             """.trimIndent()
+        }
+    }
+
+    triggers {
+        retryBuild {
+            attempts = 1
         }
     }
 })
