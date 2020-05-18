b1e037cc51391dfff651b65e7a1f6b2014fa3df6
nikolay <nikolay@TeamCity>
2020-05-18 20:59:02 +0000

    TeamCity change in 'FourthDemo / Application' project: VCS roots of 'TestRestApi' build configuration were updated

--- .teamcity/Application/buildTypes/Application_TestRestApi.kt
+++ .teamcity/Application/buildTypes/Application_TestRestApi.kt
@@ -4,4 +4,8 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 
 object Application_TestRestApi : BuildType({
     name = "TestRestApi"
+
+    vcs {
+        root(_Self.vcsRoots.Dp185DevOpsPRiS)
+    }
 })
