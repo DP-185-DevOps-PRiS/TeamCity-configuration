6fb0a45f58b2aeb8330bd0e3a95a56a631063f95
nikolay <nikolay@TeamCity>
2020-05-18 09:53:58 +0000

    TeamCity change in 'FourthDemo / Application' project: build features of 'Package' build template were updated

--- .teamcity/Application/buildTypes/Application_PackagingTemplate.kt
+++ .teamcity/Application/buildTypes/Application_PackagingTemplate.kt
@@ -1,6 +1,8 @@
 package Application.buildTypes
 
+import _Self.vcsRoots.Dp185DevOpsPRiS
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.commitStatusPublisher
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
 
 object Application_PackagingTemplate : Template({
@@ -29,6 +31,19 @@ object Application_PackagingTemplate : Template({
                 path = "/usr/share/maven"
             }
             jdkHome = "%env.JDK_11_x64%"
+        }
+    }
+
+    features {
+        commitStatusPublisher {
+            id = "BUILD_EXT_1"
+            vcsRootExtId = "${Dp185DevOpsPRiS.id}"
+            publisher = github {
+                githubUrl = "https://api.github.com"
+                authType = personalToken {
+                    token = "credentialsJSON:ecf91188-9a4b-4d29-87a5-ccb06b6fc589"
+                }
+            }
         }
     }
 })
