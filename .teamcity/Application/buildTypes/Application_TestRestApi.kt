f22c30cd82b9712a51c1551ac45a6ecff444054c
nikolay <nikolay@TeamCity>
2020-05-18 21:00:49 +0000

    TeamCity change in 'FourthDemo / Application' project: runners of 'TestRestApi' build configuration were updated

--- .teamcity/Application/buildTypes/Application_TestRestApi.kt
+++ .teamcity/Application/buildTypes/Application_TestRestApi.kt
@@ -1,7 +1,19 @@
 package Application.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
 object Application_TestRestApi : BuildType({
     name = "TestRestApi"
+
+    steps {
+        script {
+            name = "I have been executed by REST request"
+            scriptContent = """
+                echo "It works."
+                sleep 5
+                echo "..."
+            """.trimIndent()
+        }
+    }
 })
