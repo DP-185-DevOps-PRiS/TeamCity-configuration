dc5939a752117508edc5b5c8a43a448ac9434fe4
nikolay <nikolay@TeamCity>
2020-05-19 20:14:24 +0000

    TeamCity change in 'FourthDemo / Terraform' project: project parameters were changed

--- .teamcity/Terraform/Project.kt
+++ .teamcity/Terraform/Project.kt
@@ -14,6 +14,7 @@ object Project : Project({
     params {
         password("env.ARM_CLIENT_ID", "credentialsJSON:7f676f25-1a15-4eac-81a3-ebc80417c816", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("PROJECT_ID", "credentialsJSON:52eb176b-be65-4b1f-9363-137ea6254dd5", display = ParameterDisplay.HIDDEN, readOnly = true)
+        password("env.ARM_CLIENT_SECRET", "credentialsJSON:53dbf002-6b58-46b5-b977-d6bd992c1b48", display = ParameterDisplay.HIDDEN, readOnly = true)
         text("repo", "Terraform", readOnly = true, allowEmpty = true)
     }
 
