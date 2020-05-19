34c1a30e6039a5fd92922b01b65aef4de17c900f
nikolay <nikolay@TeamCity>
2020-05-19 20:15:00 +0000

    TeamCity change in 'FourthDemo / Terraform' project: project parameters were changed

--- .teamcity/Terraform/Project.kt
+++ .teamcity/Terraform/Project.kt
@@ -12,6 +12,7 @@ object Project : Project({
     buildType(Terraform_InfrastructureDestroying)
 
     params {
+        password("env.ARM_SUBSCRIPTION_ID", "credentialsJSON:21757c12-89df-4120-b4d8-c701b7a6b42b", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("env.ARM_CLIENT_ID", "credentialsJSON:7f676f25-1a15-4eac-81a3-ebc80417c816", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("PROJECT_ID", "credentialsJSON:52eb176b-be65-4b1f-9363-137ea6254dd5", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("env.ARM_CLIENT_SECRET", "credentialsJSON:53dbf002-6b58-46b5-b977-d6bd992c1b48", display = ParameterDisplay.HIDDEN, readOnly = true)
