6c16794c849a8f1b7e9e134fa2ba0d8e1def2f2a
nikolay <nikolay@TeamCity>
2020-05-19 08:58:29 +0000

    TeamCity change in 'FourthDemo / Application' project: parameters of 'Deploy app on the new server' build configuration were updated

--- .teamcity/Application/buildTypes/Application_DeployAppOnTheNewServer.kt
+++ .teamcity/Application/buildTypes/Application_DeployAppOnTheNewServer.kt
@@ -6,6 +6,10 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 object Application_DeployAppOnTheNewServer : BuildType({
     name = "Deploy app on the new server"
 
+    params {
+        password("database_password", "credentialsJSON:bbb265da-3d7d-49c7-9a25-76240f4408d6", display = ParameterDisplay.HIDDEN, readOnly = true)
+    }
+
     steps {
         script {
             name = "Send env-files"
