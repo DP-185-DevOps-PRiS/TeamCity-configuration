ef22879b54be8c76be1b0be370862541504bee80
nikolay <nikolay@TeamCity>
2020-05-19 09:02:01 +0000

    TeamCity change in 'FourthDemo / Application' project: parameters of 'Deploy app on the new server' build configuration were updated

--- .teamcity/Application/buildTypes/Application_DeployAppOnTheNewServer.kt
+++ .teamcity/Application/buildTypes/Application_DeployAppOnTheNewServer.kt
@@ -7,12 +7,13 @@ object Application_DeployAppOnTheNewServer : BuildType({
     name = "Deploy app on the new server"
 
     params {
-        password("database_username", "credentialsJSON:cf6455f3-e3ea-471f-8248-9bd2f420a755", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("mail_password", "credentialsJSON:4ec2f34f-a6be-4705-b82d-f99fece188d4", display = ParameterDisplay.HIDDEN, readOnly = true)
+        password("database_username", "credentialsJSON:cf6455f3-e3ea-471f-8248-9bd2f420a755", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("send_mail", "credentialsJSON:689f6d3a-b429-4952-b808-aaa52d462995", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("ip_tc", "credentialsJSON:60cc8de1-2624-4259-b539-7063000c6f44", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("database_password", "credentialsJSON:bbb265da-3d7d-49c7-9a25-76240f4408d6", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("token", "credentialsJSON:455457a9-9c69-4051-a31b-c913395f4ebe", display = ParameterDisplay.HIDDEN, readOnly = true)
+        password("pub_key", "credentialsJSON:fd259e79-1bac-49f4-b650-7a8a1825fac8", display = ParameterDisplay.HIDDEN, readOnly = true)
     }
 
     steps {
