0fdf258305f4394c297040e050e376bcb7b317d1
nikolay <nikolay@TeamCity>
2020-05-19 08:35:30 +0000

    TeamCity change in 'FourthDemo / Application' project: runners of 'Deploy app on the new server' build configuration were updated

--- .teamcity/Application/buildTypes/Application_DeployAppOnTheNewServer.kt
+++ .teamcity/Application/buildTypes/Application_DeployAppOnTheNewServer.kt
@@ -1,7 +1,19 @@
 package Application.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
 object Application_DeployAppOnTheNewServer : BuildType({
     name = "Deploy app on the new server"
+
+    steps {
+        script {
+            name = "Send env-files"
+            scriptContent = """
+                # Read new IP
+                # Update env-files
+                # Send them to the /opt/kickscooter
+            """.trimIndent()
+        }
+    }
 })
