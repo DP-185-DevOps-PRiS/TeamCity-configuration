43082efe3e2bb1bb284b2f5cd0f5b1f6aa828475
nikolay <nikolay@TeamCity>
2020-05-19 09:03:28 +0000

    TeamCity change in 'FourthDemo / Application' project: parameters of 'Deploy app on the new server' build configuration were updated

--- .teamcity/Application/buildTypes/Application_DeployAppOnTheNewServer.kt
+++ .teamcity/Application/buildTypes/Application_DeployAppOnTheNewServer.kt
@@ -11,6 +11,7 @@ object Application_DeployAppOnTheNewServer : BuildType({
         password("username_app", "credentialsJSON:da22594b-73c4-4496-afd6-71512fc907e7", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("mail_password", "credentialsJSON:4ec2f34f-a6be-4705-b82d-f99fece188d4", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("username_tc", "credentialsJSON:d7e1c6cc-bd2f-4ac0-879c-af7105fe4370", display = ParameterDisplay.HIDDEN, readOnly = true)
+        password("env.IDENTITY_DB", "credentialsJSON:2139cf49-8d8d-4993-bbc3-45b20f110b04", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("database_username", "credentialsJSON:cf6455f3-e3ea-471f-8248-9bd2f420a755", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("send_mail", "credentialsJSON:689f6d3a-b429-4952-b808-aaa52d462995", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("ip_tc", "credentialsJSON:60cc8de1-2624-4259-b539-7063000c6f44", display = ParameterDisplay.HIDDEN, readOnly = true)
