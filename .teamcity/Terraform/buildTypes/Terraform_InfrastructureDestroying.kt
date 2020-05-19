73d171350a256394abcd91dc7102c6ba01cff57f
nikolay <nikolay@TeamCity>
2020-05-19 20:13:12 +0000

    TeamCity change in 'FourthDemo / Terraform' project: parameters of 'Destroy infrastructure' build configuration were updated

--- .teamcity/Terraform/buildTypes/Terraform_InfrastructureDestroying.kt
+++ .teamcity/Terraform/buildTypes/Terraform_InfrastructureDestroying.kt
@@ -9,7 +9,6 @@ object Terraform_InfrastructureDestroying : BuildType({
     params {
         password("env.ARM_SUBSCRIPTION_ID", "credentialsJSON:21757c12-89df-4120-b4d8-c701b7a6b42b", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("env.ARM_TENANT_ID", "credentialsJSON:3a8befbc-d61b-4c90-ac44-e813a0a459b1", display = ParameterDisplay.HIDDEN, readOnly = true)
-        password("env.ARM_CLIENT_SECRET", "credentialsJSON:53dbf002-6b58-46b5-b977-d6bd992c1b48", display = ParameterDisplay.HIDDEN, readOnly = true)
     }
 
     vcs {
