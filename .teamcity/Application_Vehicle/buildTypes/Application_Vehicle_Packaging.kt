9df7d96e1cc87a232b3064b5386a95daa033049a
nikolay <nikolay@TeamCity>
2020-05-20 00:42:51 +0000

    TeamCity change in 'FourthDemo / Application / Vehicle' project: parameters of 'Package' build configuration were updated

--- .teamcity/Application_Vehicle/buildTypes/Application_Vehicle_Packaging.kt
+++ .teamcity/Application_Vehicle/buildTypes/Application_Vehicle_Packaging.kt
@@ -9,10 +9,6 @@ object Application_Vehicle_Packaging : BuildType({
     templates(Application.buildTypes.Application_PackagingTemplate)
     name = "Package"
 
-    params {
-        param("repo", "kick-scooter-vehicle")
-    }
-
     steps {
         exec {
             name = "Install identity-starter dependency"
