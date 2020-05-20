65ec06a8f6431b84a1baf2e4d8e1d9ced08866b8
nikolay <nikolay@TeamCity>
2020-05-20 00:42:30 +0000

    TeamCity change in 'FourthDemo / Application / Simulator' project: parameters of 'Package' build configuration were updated

--- .teamcity/Application_Simulation/buildTypes/Application_Simulation_Packaging.kt
+++ .teamcity/Application_Simulation/buildTypes/Application_Simulation_Packaging.kt
@@ -5,4 +5,8 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 object Application_Simulation_Packaging : BuildType({
     templates(Application.buildTypes.Application_PackagingTemplate)
     name = "Package"
+
+    params {
+        param("REPO", "kick-scooter-simulator")
+    }
 })
