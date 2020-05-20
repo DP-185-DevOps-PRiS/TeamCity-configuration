0b47faeafe0a22f1a6776129d2347af975aaf8b2
nikolay <nikolay@TeamCity>
2020-05-20 00:36:42 +0000

    TeamCity change in 'FourthDemo' project: 'DP-185-DevOps-PRiS' VCS root was updated

--- .teamcity/_Self/vcsRoots/Dp185DevOpsPRiS.kt
+++ .teamcity/_Self/vcsRoots/Dp185DevOpsPRiS.kt
@@ -5,6 +5,6 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
 object Dp185DevOpsPRiS : GitVcsRoot({
     name = "DP-185-DevOps-PRiS"
-    url = "https://github.com/DP-185-DevOps-PRiS/%repo%"
+    url = "https://github.com/DP-185-DevOps-PRiS/%REPO%"
     branchSpec = "+:(master)"
 })
