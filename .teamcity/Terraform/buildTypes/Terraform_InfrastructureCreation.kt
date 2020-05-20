68cf498e066779d67a5f79cd477e14a45202e0da
nikolay <nikolay@TeamCity>
2020-05-20 05:26:03 +0000

    TeamCity change in 'FourthDemo / Terraform' project: runners of 'Create infrastructure' build configuration were updated

--- .teamcity/Terraform/buildTypes/Terraform_InfrastructureCreation.kt
+++ .teamcity/Terraform/buildTypes/Terraform_InfrastructureCreation.kt
@@ -27,18 +27,6 @@ object Terraform_InfrastructureCreation : BuildType({
                 
                 terraform init -backend=true
                 terraform apply -auto-approve
-                
-                echo "Update env-files ..."
-                service_list=(gateway identity messaging payment simulator trip vehicle)
-                DB_IP=${'$'}(cat db_ip.txt)
-                mkdir env && chmod 700 env
-                cp %ENV_TEMPLATES_PATH%/kafka.env env
-                for service in ${'$'}{service_list[*]}; do
-                  sed "s|ip|${'$'}DB_IP|" %ENV_TEMPLATES_PATH%/${'$'}{service}-template.env > env/${'$'}{service}.env
-                done
-                
-                echo "Sending updated env-files to the Google Storage ..."
-                gsutil -q cp -r env/ gs://%BUCKET_NAME%/
             """.trimIndent()
         }
     }
