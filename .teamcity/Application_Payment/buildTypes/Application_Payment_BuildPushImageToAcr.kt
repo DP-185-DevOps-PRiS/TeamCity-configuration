19beffff9d6badb031f3509ed1b18dc4aa785d6d
nikolay <nikolay@TeamCity>
2020-05-20 00:47:53 +0000

    TeamCity change in 'FourthDemo / Application / Payment' project: parameters of 'Build & Push image to ACR' build configuration were updated

--- .teamcity/Application_Payment/buildTypes/Application_Payment_BuildPushImageToAcr.kt
+++ .teamcity/Application_Payment/buildTypes/Application_Payment_BuildPushImageToAcr.kt
@@ -7,10 +7,6 @@ object Application_Payment_BuildPushImageToAcr : BuildType({
     templates(Application.buildTypes.Application_Docker)
     name = "Build & Push image to ACR"
 
-    params {
-        param("service", "payment")
-    }
-
     triggers {
         finishBuildTrigger {
             id = "TRIGGER_6"
