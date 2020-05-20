0237514c4aa737221eb3e3b10b35360f63a305c6
nikolay <nikolay@TeamCity>
2020-05-20 01:25:29 +0000

    TeamCity change in 'FourthDemo / Application' project: parameters of 'Deploy' build template were updated

--- .teamcity/Application/buildTypes/Application_Deploy.kt
+++ .teamcity/Application/buildTypes/Application_Deploy.kt
@@ -15,7 +15,6 @@ object Application_Deploy : Template({
         password("server", "credentialsJSON:142c01de-b679-4822-8bd5-92ead4140cf1", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("username_app", "credentialsJSON:da22594b-73c4-4496-afd6-71512fc907e7", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("password", "credentialsJSON:14665d28-7888-421c-9e5c-6854a9ec0b1f", display = ParameterDisplay.HIDDEN, readOnly = true)
-        password("ip_tc", "credentialsJSON:60cc8de1-2624-4259-b539-7063000c6f44", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("username_tc", "credentialsJSON:d7e1c6cc-bd2f-4ac0-879c-af7105fe4370", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("user", "credentialsJSON:c2eaf1bd-00bf-4e0f-8a65-2ee1800e5a57", display = ParameterDisplay.HIDDEN, readOnly = true)
     }
